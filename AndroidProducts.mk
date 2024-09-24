63bd1f2aa3ff06fae6aa0d79417476cf9ccf9e22
ArianK16a <dhanushvozh345@gmail.com>
2024-09-23 19:45:56 -0700

    welcome to Euclidos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_sky.mk
+    $(LOCAL_DIR)/euclid_sky.mk
+
+COMMON_LUNCH_CHOICES := \
+    euclid_sky-ap2a-eng \
+    euclid_sky-ap2a-userdebug \
+    euclid_sky-ap2a-user
--- euclid_sky.mk
+++ euclid_sky.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common PixelOS configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common euclidOS configurations
+$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -23,10 +23,26 @@ $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := aosp_sky
+PRODUCT_NAME := euclid_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
+
+#Maintainer
+EUCLID_BUILD_TYPE := OFFICIAL
+EUCLID_MAINTAINER := Dhanush
+#Gapps
+EUCLID_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_GOOGLE_DIALER := true
+
+#UDFPS
+EXTRA_UDFPS_ANIMATIONS := false
+
+#Misc.
+TARGET_SUPPORTS_TOUCHGESTURES := false
+
+TARGET_SUPPORTS_BLUR := false
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
