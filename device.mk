36d02125a1f30aec84cbab8a803cb9bac2d6f934
dhanush281 <dhanushvozh345@gmail.com>
2024-09-29 03:25:43 -0700

    Enable freeform windows

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # F2FS utilities
 PRODUCT_PACKAGES += \
     sg_write_buffer \
